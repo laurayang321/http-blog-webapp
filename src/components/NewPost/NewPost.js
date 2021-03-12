cf14d14d62fd5232c017dd685723213955e7a9a7
Laura Yang <laurayang321@gmail.com>
2021-03-11 23:48:54 -0800

    167. POSTing Data to the Server

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
-        content: '',
+        body: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        // data is a JavaScript Object
+        // but axios will automatically turn this into JSON data
+        const data = {
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
@@ -16,13 +31,13 @@ class NewPost extends Component {
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.body} onChange={(event) => this.setState({body: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
