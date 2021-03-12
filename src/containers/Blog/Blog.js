1ad7d1b61ea3d7d0d86db6c21cf3a0888f6d1cfb
Laura Yang <laurayang321@gmail.com>
2021-03-11 19:04:37 -0800

    162. Creating a Http Request to GET Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,6 +7,17 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    // then() takes a function as input. 
+    // The function will get executed once the promise resolves - once the data from backend is returned.
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/post')
+            // the function then receives a response object as an input 
+            .then(resonse => {
+                console.log(response);
+            });
+    }
+
     render () {
         return (
             <div>
